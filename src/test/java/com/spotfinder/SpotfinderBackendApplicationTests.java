d24dd9a05446e8de2f35c8a13eb8de31aa069e8e
v.plotnikov <vadim345plotnikov@gmail.com>
2026-04-29 19:19:31 +0300

    Configure test profile

--- src/test/java/com/spotfinder/SpotfinderBackendApplicationTests.java
+++ src/test/java/com/spotfinder/SpotfinderBackendApplicationTests.java
@@ -2,10 +2,22 @@ package com.spotfinder;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@ActiveProfiles("test")
 @SpringBootTest
 class SpotfinderBackendApplicationTests {
 
+  @Container
+  @ServiceConnection
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+  }
 }
